18b83e1efa06f6d989ce6c52f0d4da1986181cde
formegusto <formegusto@gmail.com>
2021-01-10 10:15:09 +0900

    useLazyQuery Example Init

--- src/container/PersonsContainer.js
+++ src/container/PersonsContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
-import { useQuery } from 'react-apollo';
+import { useLazyQuery, useQuery } from 'react-apollo';
 import CreatePersonContainer from './CreatePersonContainer';
 
 const FETCH_PERSONS = gql`
@@ -13,15 +13,19 @@ const FETCH_PERSONS = gql`
 `;
 
 function PersonsContainer() {
-    const { loading, error, data, refetch} = useQuery(FETCH_PERSONS);
+    // const { loading, error, data, refetch} = useQuery(FETCH_PERSONS);
+    const [fetchPersons, { loading, error ,data, refetch}] = useLazyQuery(FETCH_PERSONS);
 
     if(loading) return <p>Loading...</p>
     if(error) return <p>Error...</p>
     return (
         <>
             <h2>Persons</h2>
+            <button onClick={() => fetchPersons()}>
+                불러오기
+            </button>
             <ul>
-                {data.persons.map(({ id, name }) => (
+                {data && data.persons.map(({ id, name }) => (
                     <li key={id}>{name}</li>
                 ))}
             </ul>
